aede2d0733a3f7c8d963adc63ed806bb3f5c3550
Владислав <kant1755@mail.ru>
2023-06-18 18:23:11 +0300

    base.html

--- products/views.py
+++ products/views.py
@@ -19,15 +19,15 @@ def products(request):
             },
             {
             'image': '/static/vendor/img/products/Adidas-hoodie.png',
-            'name': 'Худи черного цвета с монограммами adidas Originals',
-            'price': 6090,
-            'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            'name': 'Штаны черного цвета с монограммами adidas Originals',
+            'price': 2090,
+            'description': 'Мягкая ткань для штанов. Стиль и комфорт – это образ жизни.'
             },
             {
             'image': '/static/vendor/img/products/Adidas-hoodie.png',
-            'name': 'Худи черного цвета с монограммами adidas Originals',
-            'price': 6090,
-            'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            'name': 'Кроссовки черного цвета с монограммами adidas Originals',
+            'price': 7050,
+            'description': 'Натуральная кожа. Стиль и комфорт – это образ жизни.'
             },
         ]
     }

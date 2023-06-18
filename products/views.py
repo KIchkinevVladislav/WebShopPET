248a27d8a222995bf5b39ad26c9a0715758802f0
Владислав <kant1755@mail.ru>
2023-06-18 17:29:39 +0300

    plus static

--- products/views.py
+++ products/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store',
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - каталог',
+        'products': [
+            {
+            'image': '/static/vendor/img/products/Adidas-hoodie.png',
+            'name': 'Худи черного цвета с монограммами adidas Originals',
+            'price': 6090,
+            'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            },
+            {
+            'image': '/static/vendor/img/products/Adidas-hoodie.png',
+            'name': 'Худи черного цвета с монограммами adidas Originals',
+            'price': 6090,
+            'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            },
+            {
+            'image': '/static/vendor/img/products/Adidas-hoodie.png',
+            'name': 'Худи черного цвета с монограммами adidas Originals',
+            'price': 6090,
+            'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
+            },
+        ]
+    }
+    return render(request, 'products/products.html', context)

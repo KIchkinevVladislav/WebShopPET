33d45a5c2c984e281f87a0801f72f1011b5f8048
Владислав <kant1755@mail.ru>
2023-06-19 01:56:17 +0300

    add images

--- store/settings.py
+++ store/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_dump_load_utf8',
     
     'products',
 ]
@@ -124,7 +125,12 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static/',
     ]
 
+MEDIA_URL= '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 

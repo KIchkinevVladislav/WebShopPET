97884d18d6594ec468228d0320607f41c7c87362
Владислав <kant1755@mail.ru>
2023-06-22 01:58:18 +0300

    add profile

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from users.views import login, registrations
+from users.views import login, registrations, profile
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('registrations/', registrations, name='registrations'),
+    path('profile/', profile, name='profile'),
 ]

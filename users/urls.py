f1c9ed3106a527fb455fdde9279dd05bdc3eaed0
Владислав <kant1755@mail.ru>
2023-06-22 03:24:05 +0300

    добавлена регистрация и обработка ошибок

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from users.views import login, registrations, profile
+from users.views import login, registrations, profile, logout
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
     path('registrations/', registrations, name='registrations'),
     path('profile/', profile, name='profile'),
 ]

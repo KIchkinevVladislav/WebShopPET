98e74ae54443a590b4ff00b976ab942da3679a98
Владислав <kant1755@mail.ru>
2023-06-22 23:41:34 +0300

    add basket (model, view, url, temlate)

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class ProductCategory(models.Model):
     name = models.CharField(
@@ -16,7 +18,6 @@ class ProductCategory(models.Model):
     def __str__(self) -> str:
         return self.name
     
-
     class Meta():
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории товаров'
@@ -39,16 +40,46 @@ class Product(models.Model):
         )
     image = models.ImageField(
         upload_to='products_images',
-        verbose_name='Изображение товара')
+        verbose_name='Изображение товара'
+        )
     category = models.ForeignKey(
         to=ProductCategory, 
         on_delete=models.PROTECT,
-        verbose_name='Категория товара')
+        verbose_name='Категория товара'
+        )
     
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
 
-
     class Meta():
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(
+        to=User, 
+        on_delete=models.CASCADE, 
+        verbose_name='Пользователь'
+        )
+    product = models.ForeignKey(
+        to=Product,
+        on_delete=models.CASCADE,
+        verbose_name='Товар'
+        )
+    quantity = models.PositiveSmallIntegerField(
+        default=0, 
+        verbose_name='Количество товаров'
+        )
+    created_timestamp = models.DateField(
+        auto_now_add=True, 
+        verbose_name='Дата создания'
+        )
+    
+    def __str__(self):
+        return f'Корзина для пользователя {self.user.username} | Товары: {self.product.name}'
+    
+    class Meta():
+        verbose_name = 'Корзина с товарами'
+        verbose_name_plural = 'Корзины с товарами'
+    
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
-from .models import ProductCategory, Product
+from .models import ProductCategory, Product, Basket
+from users.models import User
+
 
 def index(request):
     context = {
@@ -16,3 +18,24 @@ def products(request):
         'categories': ProductCategory.objects.all()
     }
     return render(request, 'products/products.html', context)
+
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+def backet_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])

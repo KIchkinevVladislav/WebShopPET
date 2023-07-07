ee29195fa63e8e6007d145acde55d8bd4abfc8d8
Владислав <kant1755@mail.ru>
2023-07-08 01:32:09 +0300

    productview

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from typing import Any, Dict
+from django.db.models.query import QuerySet
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
 from .models import ProductCategory, Product, Basket
 
@@ -16,19 +18,22 @@ class IndexView(TemplateView):
         return context
 
 
-def products(request, category_id=None, page_number=1):
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    
-    per_page = 3
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
 
-    context = {
-        'title': 'Stroe - Каталог',
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator
-    }
-    return render(request, 'products/products.html', context)
+    def get_queryset(self):
+        queryset = super(ProductListView, self).get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+    
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductListView, self).get_context_data()
+        context['title'] = 'Store - Каталог'
+        context['categories'] = ProductCategory.objects.all()
+        return context
 
 
 @login_required

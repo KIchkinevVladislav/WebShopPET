2609f80ac3f05ffe2d1295a3f4cf782effae0496
Владислав <kant1755@mail.ru>
2023-07-09 00:32:24 +0300

    login/out cbv

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from users.views import login, UserRegistrarionView, UserProfileView, logout
+from users.views import UserLoginView, UserRegistrarionView, UserProfileView, logout
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', login, name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', logout, name='logout'),
     path('registrations/', UserRegistrarionView.as_view(), name='registrations'),
     path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
--- users/views.py
+++ users/views.py
@@ -1,28 +1,36 @@
-from typing import Any, Dict, Optional
-from django.db import models
+
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse, reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.views import LoginView
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
 
-def login(request):
-    if request.method == 'POST':
-        form=UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            if user:
-                auth.login(request, user)  
-                return HttpResponseRedirect(reverse('index'))             
-    else:
-        form = UserLoginForm
-    context = {'form': form}
-    return render(request, 'users/login.html', context)
+
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+# def login(request):
+#     if request.method == 'POST':
+#         form=UserLoginForm(data=request.POST)
+#         if form.is_valid():
+#             username = request.POST['username']
+#             password = request.POST['password']
+#             user = auth.authenticate(username=username, password=password)
+#             if user:
+#                 auth.login(request, user)  
+#                 return HttpResponseRedirect(reverse('index'))             
+#     else:
+#         form = UserLoginForm
+#     context = {'form': form}
+#     return render(request, 'users/login.html', context)
 
 
 class UserRegistrarionView(CreateView):

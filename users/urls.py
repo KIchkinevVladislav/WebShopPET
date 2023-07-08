fb3efe19ff959168b9b04e5c75531cf4981d241c
Владислав <kant1755@mail.ru>
2023-07-08 23:41:33 +0300

    registration cbv

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from users.views import login, registrations, profile, logout
+from users.views import login, UserRegistrarionView, profile, logout
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
-    path('registrations/', registrations, name='registrations'),
+    path('registrations/', UserRegistrarionView.as_view(), name='registrations'),
     path('profile/', profile, name='profile'),
 ]

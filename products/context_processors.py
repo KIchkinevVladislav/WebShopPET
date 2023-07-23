586d5af4806dd088478fe3ad00271a58d225d931
Владислав <kant1755@mail.ru>
2023-07-23 15:59:52 +0300

    IndexViewTestCase

--- products/context_processors.py
+++ products/context_processors.py
@@ -3,8 +3,8 @@ from products.models import Basket
 
 def baskets(request):
     """
-    Контекстный процессор 
-    Добавляем в контекс шаблона доступ к объектам Baskets 
+    Контекстный процессор
+    Добавляем в контекс шаблона доступ к объектам Baskets
     """
     user = request.user
     return {'baskets': Basket.objects.filter(user=user) if user.is_authenticated else []}
